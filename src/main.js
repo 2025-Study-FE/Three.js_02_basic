ec9751cb93093e9e81ba20a8a0906e053cade09a
seunghyeonKang <number42isuser@gmail.com>
2025-02-11 18:56:49 +0900

    -

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 // import example from "./ex01"; // three.js의 기본 구성
-import example from "./ex02";
+// import example from "./ex02"; // 캔버스, 색, 빛 설정
+import example from "./ex03"; // 애니메이션
 
 example();
